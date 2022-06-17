fdc236ab8cce3e1a685d4ac81594a34798d7405a
Zeimi <contato.brunodurao@gmail.com>
2022-06-16 22:35:28 -0300

    Tomi

--- projetoapi/api/urls.py
+++ projetoapi/api/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
-
-urlpatterns = {
-    path('', views.index, name=('index')),
-    path('buscar/', views.buscar),
-    path('criar/', views.criar),
-
-}
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('buscar/', views.buscar, name='buscar'),
+    path('criar/', views.criar, name='criar'),
+    path('inserir/', views.inserir, name='inserir'),
+]
--- projetoapi/api/views.py
+++ projetoapi/api/views.py
@@ -1,35 +1,43 @@
 import json
-from django.http import JsonResponse
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
 from api.models import Pokemon
 
-# Create your views here.
 
 def index(request):
-    return render (request, 'index.html')
+    return render(request, 'index.html')
+
+def inserir(request):
+    return render(request, 'inserir.html')
 
 def buscar(request):
     try:
         pokemons = []
-        lista = {}
+        data = {}
 
-        for poke in Pokemon.objects.all():
-            pokelista = {}
-            pokelista['nome'] = poke.nome
-            pokelista['tipo'] = poke.tipo
+        for item in list(Pokemon.objects.all()):
+            poke = {}
+            poke['nome'] = item.nome
+            poke['tipo'] = item.tipo
 
-            pokemons.append(pokelista)
-        
-        lista ['lista'] = json.dumps(pokemons)
+            pokemons.append(poke)
 
-        return JsonResponse(lista)
+        data['data'] = json.dumps(pokemons)
+
+        return JsonResponse(data)
     except Exception as error:
         valores = {}
-        print ('Erro', error)
+        print('Error', error)
 
-        valores = {'Erro': 'Ocorreu um erro'}
+        valores = {
+            'Erro': 'Ocorreu um erro'
+        }
+
         valores = json.dumps(valores)
+
         return JsonResponse(valores)
+
 
 def criar(request):
     try:
@@ -37,7 +45,6 @@ def criar(request):
             nome = ''
             tipo = ''
             intermediaria = json.loads(request.body.decode('utf-8'))
-
             nome = intermediaria['nome']
             tipo = intermediaria['tipo']
 
@@ -47,16 +54,20 @@ def criar(request):
 
             pokemon.save()
 
-            sucesso = {'nome': nome, 'tipo': tipo}
+            sucesso = {
+                'nome': nome,
+                'tipo': tipo
+            }
+
             return JsonResponse(sucesso)
-
         else:
-            outro = {'msg': 'Erro ao tentar acessar a api'}
+            outro = {
+                'msg': 'Erro ao tentar acessar a api'
+            }
             return JsonResponse(outro)
-
     except Exception as error:
         print('Error', error)
 
         nada = {}
-        nada ['erro'] = 'Ocorreu algum erro no sistema'
+        nada['erro'] = 'Ocorreu algum erro no sistema'
         return JsonResponse(nada)
