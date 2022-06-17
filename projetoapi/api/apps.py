676b91aad484af728fb4813f1f6b0234622bdd23
Zeimi <contato.brunodurao@gmail.com>
2022-06-17 18:11:46 -0300

    Comentários 50%

--- projetoapi/api/apps.py
+++ projetoapi/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class ApiConfig(AppConfig): # Configuração da aplicação padrão do Django
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
--- projetoapi/api/models.py
+++ projetoapi/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Pokemon(models.Model):
-    nome = models.TextField(max_length=255)
-    tipo = models.TextField(max_length=255)
+class Pokemon(models.Model): # Modelo de Pokemon para o Banco de Dados
+    nome = models.TextField(max_length=255) # Nome do Pokemon, como string
+    tipo = models.TextField(max_length=255) # Tipo do Pokemon, como string
